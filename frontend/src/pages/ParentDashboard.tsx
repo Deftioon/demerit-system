97fcdee2a7cc3748afab268b4a7b64cba04e4de6
Jake Lu <jylu1205@outlook.com>
2025-02-25 10:59:26 +0800

    bhi

--- frontend/src/pages/ParentDashboard.tsx
+++ frontend/src/pages/ParentDashboard.tsx
@@ -1,11 +1,14 @@
 import { useUser } from "../contexts/UserContext";
+import "./ParentDashboard.css";
 
 export const ParentDashboard = () => {
   const { user } = useUser();
   return (
     <div>
       <h1>Parent Dashboard</h1>
-      <p>Welcome, {user?.firstName}!</p>
+      <div className="welcome">
+        <h3>Welcome, {user?.email}!</h3>
+      </div>
     </div>
   );
 };
